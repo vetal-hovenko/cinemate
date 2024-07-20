5631c302e1ee7740e6864733840b3fa62fa1a251
vetal_hovenko <vet.hov.main@gmail.com>
2024-07-20 19:16:28 +0300

    improved watcth list screen UI

--- src/screens/WatchListScreen.tsx
+++ src/screens/WatchListScreen.tsx
@@ -5,23 +5,42 @@ import WatchList from "../components/WatchList/WatchList";
 import MoviesSortOrderSelect, {
     SortKey,
 } from "../components/UI/MoviesSortOrderSelect/MoviesSortOrderSelect";
+import { useSelector } from "../redux/hooks";
+import StyledText, { TextSize } from "../components/UI/StyledText/StyledText";
 
 const WatchListScreen = () => {
+    const movies = useSelector((store) => store.movies.movies);
+
     const [sortKey, setSortKey] = useState<SortKey>("default");
     const [isOrderAscending, setIsOrderAscending] = useState(true);
 
     return (
-        <ScrollView style={styles.scrollView}>
+        <ScrollView
+            style={styles.scrollView}
+        >
             <View style={styles.container}>
-                <MoviesSortOrderSelect
-                    isOrderAscending={isOrderAscending}
-                    setIsOrderAscending={setIsOrderAscending}
-                    setSortKey={setSortKey}
-                />
-                <WatchList
-                    isOrderAscending={isOrderAscending}
-                    sortKey={sortKey}
-                />
+                {!movies.length ? (
+                    <View className="flex-1 justify-center items-center mt-56">
+                        <StyledText
+                            style={{ opacity: 0.5 }}
+                            size={TextSize.LARGE}
+                        >
+                            No movies yet
+                        </StyledText>
+                    </View>
+                ) : (
+                    <>
+                        <MoviesSortOrderSelect
+                            isOrderAscending={isOrderAscending}
+                            setIsOrderAscending={setIsOrderAscending}
+                            setSortKey={setSortKey}
+                        />
+                        <WatchList
+                            isOrderAscending={isOrderAscending}
+                            sortKey={sortKey}
+                        />
+                    </>
+                )}
             </View>
         </ScrollView>
     );
